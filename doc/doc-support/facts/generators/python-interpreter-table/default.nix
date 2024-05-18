0030db4fd925a2655b04e10c6b72f4c74ab1e7a5
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2024-05-18 19:45:38 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! doc: WIP fact injection

--- doc/doc-support/facts/generators/python-interpreter-table/default.nix
+++ doc/doc-support/facts/generators/python-interpreter-table/default.nix
@@ -2,25 +2,15 @@
 # this would simplify the generation of the bash injector,
 # but makes it hard to explore the facts.
 { lib
-, stdenv
-, writeScriptBin
+, runCommand
 # Configuration input
 , id
 }:
 let
-  inherit (lib.strings) readFile;
   contentFile = (./. + "/content.md");
 in {
-  drv = stdenv.mkDerivation {
-    name = "FACT_${id}";
-    src = ./.;
-    phases = [ "installPhase" ];
-    installPhase = ''
-      dest="$out"
-      mkdir -p "$dest"
-
-      cp ${contentFile} $dest/${id}.md
-    '';
-  };
+  drv = runCommand "FACT_${id}.md" { inherit contentFile; } ''
+    cp $contentFile $out
+  '';
   target = "./languages-frameworks/python.section.md";
 }
